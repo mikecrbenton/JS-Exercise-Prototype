a37a52bf18ed08ad28f57fa1ff9f001b7f4cb1d4
mikecrbenton <mikecrbenton@hotmail.com>
2020-05-04 22:02:19 -0500

    can't get the last 2 stretch problems

--- index.js
+++ index.js
@@ -58,19 +58,22 @@ Person.prototype.toString = function (){
   return `${this.name}, ${this.age}`;
 };
 
-/*
+/*======================================================================================================
   TASK 2
     - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
     - All instances built with Car:
         + should initialize with an `tank` at 0
         + should initialize with an `odometer` at 0
     - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
+
     - STRETCH: Give cars ability to `.drive(distance)`. The distance driven:
         + Should cause the `odometer` to go up.
         + Should cause the the `tank` to go down taking `milesPerGallon` into account.
+
     - STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
-*/
+
+*///=====================================================================================================
 
 function Car( model, milesPerGallon) {
   this.model = model;
@@ -82,6 +85,22 @@ function Car( model, milesPerGallon) {
 Car.prototype.fill = function( gallons ){
   this.tank += gallons;
 };
+
+Car.prototype.drive = function( distance ){
+
+      //console.log(`distance is ${distance}`);
+
+  this.odometer += distance;
+  this.tank -= ( Math.round(distance / this.milesPerGallon) ) ;
+
+      //console.log( `${this.tank} -= (Math.round(${distance} / ${this.milesPerGallon})`); 
+
+  if(this.tank <= 0){
+    return `I ran out of fuel at ${this.odometer} miles!`;
+  }
+
+};
+
 
 /*=============================================================================================
   TASK 3
@@ -96,7 +115,7 @@ function Baby( babyargs, num, favoriteToy ) {
   Person.call(this, babyargs, num );
 
   this.favoriteToy = favoriteToy ;
-  
+
 }
 
 //LINK THE PERSON PROTOTYPE WITH THE BABY PROTOTYPE
